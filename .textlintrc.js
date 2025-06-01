38a77ce3b31b8d4822f41fb6402ae77db61032da
KUBOTA Shota <ta2@o2p.jp>
2025-06-01 12:07:58 +0900

    chore(textlint): update textlint settings

--- .textlintrc.js
+++ .textlintrc.js
@@ -5,5 +5,9 @@ module.exports = {
   rules: {
     ...config.rules,
     'no-dead-link': false,
+    'preset-jtf-style': {
+      '1.1.3.箇条書き': false,
+      '3.1.2.全角文字どうし': false,
+    },
   },
 };
